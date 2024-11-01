792a33335675f341b755fd75fba1ba21078fe01e
Fabian Velasquez <fabianvegadev@gmail.com>
2024-11-01 00:08:46 -0500

    Ajustes en el ancho de componentes

--- src/Data/Levels.jsx
+++ src/Data/Levels.jsx
@@ -1,6 +1,10 @@
 export const levels = {
-    Beginner: 25,
-    Intermediate: 50,
-    Advanced: 75,
-    Expert: 100,
+    Beginner1: 12.5,
+    Beginner2: 25,
+    Intermediate1: 37.5,
+    Intermediate2: 50,
+    Advanced1: 67.5,
+    Advanced2: 75,
+    Expert1: 87.5,
+    Expert2: 100,
 };
